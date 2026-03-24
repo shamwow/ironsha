de836a023c92d0ebf332995e07847361401db5f7
Shahmeer Navid <shahmeernavid@gmail.com>
2026-03-24 18:13:32 -0400

    Fix logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,17 @@
 import pino from "pino";
 import { config } from "./config.js";
 
-export const logger = pino({ level: config.LOG_LEVEL });
+const logDir = process.env.LOG_DIR || "./logs";
+
+export const logger = pino({
+  level: config.LOG_LEVEL,
+  transport: {
+    target: "pino-roll",
+    options: {
+      file: `${logDir}/ironsha`,
+      frequency: "daily",
+      extension: ".log",
+      limit: { count: 7 },
+    },
+  },
+});
